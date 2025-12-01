977159f20a40e3a460464cb16c6590a46e228029
Erza Bytyci <erzabytyqi17@gmail.com>
2025-12-01 23:34:35 +0200

    Exercise 10.12: enviroment variables

---
+++ src/utils/apolloClient.js
@@ -0,0 +1,13 @@
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+import Constants from 'expo-constants';
+
+const { apolloUri } = Constants.expoConfig.extra;
+
+const createApolloClient = () => {
+  return new ApolloClient({
+    uri: apolloUri,
+    cache: new InMemoryCache(),
+  });
+};
+
+export default createApolloClient;
