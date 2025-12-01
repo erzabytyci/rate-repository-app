e370e747992a52077a808416c45646b2e05a6951
Erza Bytyci <erzabytyqi17@gmail.com>
2025-12-01 23:22:39 +0200

    Exercise 10.11: fetching repositories with Apollo Client

--- App.js
+++ App.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { NativeRouter } from 'react-router-native';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+} from '@apollo/client';
+
 import Main from './src/components/Main';
+
+const client = new ApolloClient({
+  uri: 'http://10.0.2.2:4000/graphql',
+  cache: new InMemoryCache(),
+});
 
 export default function App() {
   return (
     <>
+    <ApolloProvider client={client}>
       <NativeRouter>
-      <Main />
-      <StatusBar style="auto" />
-    </NativeRouter>
+        <Main />
+          <StatusBar style="auto" />
+      </NativeRouter>
+     </ApolloProvider>
     </>
   );
 }
