8fe6b0d89aea43198f851d8f1920d27fbc893967
Erza Bytyci <erzabytyqi17@gmail.com>
2025-12-03 00:14:32 +0200

    Exercise 10.16: sign out

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import { View, StyleSheet, ScrollView} from 'react-native';
+import { View, StyleSheet, ScrollView, Pressable} from 'react-native';
 import Constants from 'expo-constants';
-import { Link } from 'react-router-native';
+import { Link, useNavigate } from 'react-router-native';
+import { useQuery, useApolloClient } from '@apollo/client';
 
 import Text from './Text';
 import theme from '../theme';
+import { ME } from '../graphql/queries';
+import useAuthStorage from '../hooks/useAuthStorage';
 
 const styles = StyleSheet.create({
   container: {
@@ -22,7 +25,9 @@ const styles = StyleSheet.create({
   },
 });
 
-const AppBarTab = ({ to, children }) => {
+const AppBarTab = ({ to, children, onPress }) => {
+
+  if (to) {
   return (
     <Link to={to} style={styles.tab}>
       <Text
@@ -34,14 +39,47 @@ const AppBarTab = ({ to, children }) => {
       </Text>
     </Link>
   );
+}
+
+ return (
+    <Pressable onPress={onPress} style={styles.tab}>
+      <Text
+        style={{ color: 'white' }}
+        fontWeight="bold"
+        fontSize="subheading"
+      >
+        {children}
+      </Text>
+    </Pressable>
+  );
 };
 
 const AppBar = () => {
+  const { data } = useQuery(ME, {
+    fetchPolicy: 'cache-and-network',
+  });
+
+  const authStorage = useAuthStorage();
+  const apolloClient = useApolloClient();
+  const navigate = useNavigate();
+
+  const signOut = async () => {
+    await authStorage.removeAccessToken();
+    await apolloClient.resetStore();
+    navigate('/');
+  };
+
+  const isSignedIn = !!data?.me;
+
   return (
     <View style={styles.container}>
         <ScrollView horizontal style={styles.scroll} showsHorizontalScrollIndicator={false}>
            <AppBarTab to="/">Repositories</AppBarTab>
-           <AppBarTab to="/signin">Sign in</AppBarTab>
+            {isSignedIn ? (
+              <AppBarTab onPress={signOut}>Sign out</AppBarTab>
+            ) : (
+              <AppBarTab to="/signin">Sign in</AppBarTab>
+            )}
            </ScrollView>
     </View>
   );
